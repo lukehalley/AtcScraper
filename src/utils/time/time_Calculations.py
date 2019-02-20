3273382118d5571502cd637e4307222a19fa79d8
Luke Halley <luke123halley@gmail.com>
2019-02-20 12:09:18 +1000

    Add comment about time calculation methods

--- src/utils/time/time_Calculations.py
+++ src/utils/time/time_Calculations.py
@@ -7,6 +7,7 @@ This module provides functions for formatting dates and durations
 Provides conversion and scheduling helpers for scraping tasks.
 # Handle UTC timestamp conversions for consistency
 # Ensure timestamps maintain millisecond precision for accurate reporting
+# Time conversion and interval calculation helpers
 """
 according to configurable format strings stored in environment variables.
 """Handle time-based calculations and conversions.
