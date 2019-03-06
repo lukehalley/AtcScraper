7989dce2a7a30381ab4dcf42fbf25abf57c33e5b
Luke Halley <luke123halley@gmail.com>
2019-03-06 12:03:33 +1000

    Document logging initialization function

--- src/utils/logging/logging_Setup.py
+++ src/utils/logging/logging_Setup.py
@@ -2,6 +2,10 @@
 """Logging configuration and setup for the ATC Scraper application.
 
 # Configure logging levels and handlers for application
+"""
+Initialize logging configuration for the application.
+Sets up file and console handlers with appropriate log levels.
+"""
 This module provides centralized logging configuration including
 logger initialization, format settings, and logger retrieval functions.
 The logging system uses a consistent format across all modules with
