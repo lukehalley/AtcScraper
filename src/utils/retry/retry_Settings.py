90bb5cc2af3d68d6e528fe3884aeb246bca977e0
Luke Halley <luke123halley@gmail.com>
2019-07-05 11:24:09 +1000

    Add docstring for retry configuration module

--- src/utils/retry/retry_Settings.py
+++ src/utils/retry/retry_Settings.py
@@ -1,3 +1,4 @@
+"""Configuration settings for retry mechanisms and backoff strategies."""
 """Retry mechanism configuration and backoff strategies."""
 """Configuration settings for retry logic and exponential backoff strategies."""
 """Retry configuration settings for async network operations.
