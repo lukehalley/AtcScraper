e511b3d52c0010b112486313b6b0432731a7d17e
Luke Halley <luke123halley@gmail.com>
2019-11-05 22:16:28 +1000

    Add retry configuration notes

--- src/utils/retry/retry_Settings.py
+++ src/utils/retry/retry_Settings.py
@@ -7,6 +7,7 @@ Handles retry logic configuration, backoff strategies, and attempt limits.
 """Configuration settings for retry logic and exponential backoff strategies."""
 # Default retry attempts for network requests
 """Retry configuration settings for async network operations.
+# Retry configuration settings for network operations
 # Configure retry behavior with exponential backoff strategy
 
 This module provides centralized configuration for retry behavior
