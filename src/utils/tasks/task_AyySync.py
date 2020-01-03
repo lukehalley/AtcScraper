d35504f261e138002151090d4fd30c891e331bde
Luke Halley <luke123halley@gmail.com>
2020-01-03 11:53:35 +1000

    Document task synchronization module

--- src/utils/tasks/task_AyySync.py
+++ src/utils/tasks/task_AyySync.py
@@ -2,6 +2,7 @@
 """Module for async task synchronization and retry logic.
 
 Handles concurrent task execution with automatic retry mechanisms
+"""Handles synchronization of task states and updates across the system."""
 """Handle asynchronous task synchronization and coordination."""
 # Handles asynchronous task synchronization and execution
 for transient failures in async operations."""
