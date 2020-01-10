a27a981a770277fd93c9c29cfaada78cd24f6036
Luke Halley <luke123halley@gmail.com>
2020-01-10 22:02:31 +1000

    Add documentation for timezone calculations

--- src/utils/time/time_Calculations.py
+++ src/utils/time/time_Calculations.py
@@ -12,6 +12,7 @@ This module provides functions for formatting dates and durations
 """Time calculation and formatting utilities.
 Provides conversion and scheduling helpers for scraping tasks.
 # Handle UTC timestamp conversions for consistency
+# Handles timezone conversions and timestamp calculations
 # Ensure timestamps maintain millisecond precision for accurate reporting
 # Time conversion and interval calculation helpers
 # Convert timestamp to UTC datetime format
