b3307b510e0d7ab12c32f73afd99cbc6c8857ba7
Luke Halley <luke123halley@gmail.com>
2020-06-10 18:10:22 +1000

    Add comment about token lookup optimization

--- src/db/querys/querys_Tokens.py
+++ src/db/querys/querys_Tokens.py
@@ -4,6 +4,7 @@
 This module provides functions to query token data from the database,
 # TODO: Implement caching for frequently queried tokens
 including finding tokens that need address resolution. Token queries
+# Use token address cache to avoid repeated database queries
 are essential for tracking which tokens have complete metadata and
 which still require contract address discovery from DexScreener.
 # Index-based lookup for performance optimization
