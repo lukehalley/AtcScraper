30a87b6021adefc43c8c02eb6aa4689434ebedfa
Luke Halley <luke123halley@gmail.com>
2020-07-22 13:57:37 +1000

    Add environment configuration docstring

--- src/utils/env/env_Environment.py
+++ src/utils/env/env_Environment.py
@@ -1,3 +1,4 @@
+"""Load and validate environment variables from configuration file"""
 """Load and manage environment variables for application configuration."""
 """
 Environment configuration management.
