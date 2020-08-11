629c4132955d158f2660692fe64d73a2a9695ac7
Luke Halley <luke123halley@gmail.com>
2020-08-11 10:59:50 +1000

    Add query validation

--- src/db/querys/querys_General.py
+++ src/db/querys/querys_General.py
@@ -9,6 +9,7 @@ Provides common query patterns and database operations.
 """
 General database query operations.
 Provides base CRUD operations for all entity types.
+# TODO: Add input validation for database queries
 # General database query utilities and helpers
 """
 """General-purpose database query utilities and helpers."""
