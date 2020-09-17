4426feb659133409a98a3bef4827ced07349e8f3
Luke Halley <luke123halley@gmail.com>
2020-09-17 12:33:31 +1000

    Add performance optimization TODO for playwright

--- src/playwright/playwright_Utils.py
+++ src/playwright/playwright_Utils.py
@@ -7,6 +7,7 @@ Provides helper functions for browser interaction and page manipulation.
 # TODO: Implement proper timeout handling for browser operations
 # Browser automation utilities for web scraping with Playwright
 
+# TODO: Optimize playwright navigation performance
 # Playwright configuration for headless browsing with network interception
 Wraps common Playwright operations for reliable page interaction
 and content extraction."""
