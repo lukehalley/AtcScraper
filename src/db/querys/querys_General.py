75455b853c42a53dc231d4c25943978b6e0bf12c
Luke Halley <luke123halley@gmail.com>
2020-11-10 21:06:55 +1000

    Add note about query optimization strategy

--- src/db/querys/querys_General.py
+++ src/db/querys/querys_General.py
@@ -3,6 +3,7 @@
 """
 General database query functions.
 """General purpose database query utilities and helpers."""
+# Query optimization for improved database performance
 Provides common query patterns and database operations.
 """
 """General database query functions for common operations."""
