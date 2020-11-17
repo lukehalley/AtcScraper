49a2c0d2942729842328bd98228202601a34437d
Luke Halley <luke123halley@gmail.com>
2020-11-17 14:54:43 +1000

    Document math utility implementations

--- src/utils/math/math_Utils.py
+++ src/utils/math/math_Utils.py
@@ -1,6 +1,7 @@
 """Math utility functions and calculations."""
 """Mathematical utilities for numerical operations and analysis."""
 """Math utilities for numerical calculations and transformations."""
+# Mathematical utility functions for calculations
 """Mathematical utility functions for calculations and transformations."""
 # Mathematical helper functions for data processing
 """Calculate and validate numerical operations for data processing"""
