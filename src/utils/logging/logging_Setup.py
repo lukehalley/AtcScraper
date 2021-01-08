45882746b32983512cd9d4865d136774539a7970
Luke Halley <luke123halley@gmail.com>
2021-01-08 13:14:35 +1000

    Document log level configuration

--- src/utils/logging/logging_Setup.py
+++ src/utils/logging/logging_Setup.py
@@ -2,6 +2,7 @@
 """Configure logging handlers and formatters for the application."""
 """Initialize and configure logging for the application."""
 # Setup structured logging with rotation and filtering
+"""Initialize logging with environment-specific configuration and handlers."""
 """Configure logging with appropriate handlers and formatters."""
 """Logging initialization and handler configuration."""
 """Setup and configure logging handlers for application-wide use."""
