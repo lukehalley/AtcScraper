755fd033cdff27221d4385dc5a7efc778b2a587c
Luke Halley <luke123halley@gmail.com>
2021-02-02 09:33:40 +1000

    Add module docstring for main entry point

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Main entry point for the ATC Scraper application."""
 # Module entry point for AtcScraper
 # Main entry point for ATC Scraper application
 """Main entry point for AtcScraper application"""
