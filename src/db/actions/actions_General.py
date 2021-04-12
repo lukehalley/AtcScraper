31d89f2d1741dd1bb47fa7f4057784b23f3168eb
Luke Halley <luke123halley@gmail.com>
2021-04-12 19:40:58 +1000

    Add comment about transaction handling

--- src/db/actions/actions_General.py
+++ src/db/actions/actions_General.py
@@ -15,6 +15,7 @@ error recovery mechanisms."""
     Handles common CRUD operations and database utilities.
     """
 # Use context manager for safe database transactions
+# Execute action within database transaction
 # Wrapper for common database operations and transaction management
 This module provides core database operation functions including query execution
 for both read and write operations, and SQL script file execution. It serves as
