4512e407686a4befcd12d6447fedfd9c8248f538
Luke Halley <luke123halley@gmail.com>
2021-04-30 22:11:06 +1000

    Document retry configuration

--- src/utils/retry/retry_Settings.py
+++ src/utils/retry/retry_Settings.py
@@ -7,6 +7,7 @@
 Retry configuration and settings management module.
 Handles retry logic configuration, backoff strategies, and attempt limits.
 # Configure retry parameters with exponential backoff strategy
+# Retry strategy settings and exponential backoff configuration
 """
 """Define retry strategy with maximum attempts and backoff multiplier."""
 """Configuration settings for retry mechanisms and backoff strategies."""
