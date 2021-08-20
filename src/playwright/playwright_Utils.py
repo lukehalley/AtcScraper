08d3d31898d670564eeae7185d637580381f9057
Luke Halley <luke123halley@gmail.com>
2021-08-20 14:44:06 +1000

    Document browser automation helper functions

--- src/playwright/playwright_Utils.py
+++ src/playwright/playwright_Utils.py
@@ -4,6 +4,7 @@ Playwright browser automation utilities.
 Provides helper functions for browser interaction and page manipulation.
 """
 """Playwright automation utilities for web scraping and browser interaction."""
+# Browser automation utilities for page navigation and interaction
 """Playwright browser automation utilities.
 # TODO: Implement proper timeout handling for browser operations
 # Browser automation utilities for web scraping with Playwright
