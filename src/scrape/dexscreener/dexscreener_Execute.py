ec88a89ea1b5536a961d561e6e08c08c61caf8fc
Luke Halley <luke123halley@gmail.com>
2021-08-20 10:44:21 +1000

    Document main execution flow and control logic

--- src/scrape/dexscreener/dexscreener_Execute.py
+++ src/scrape/dexscreener/dexscreener_Execute.py
@@ -8,6 +8,7 @@ Dexscreener scraping execution module.
 Orchestrates the scraping pipeline and handles execution flow.
 """
 """Execute scraping tasks for dexscreener data sources."""
+"""Orchestrate the main execution flow: initialization, scraping, and error handling."""
 """Execute scraping operations with retry logic and error recovery mechanisms."""
 """Execute dexscreener scraping operations."""
 """Main execution logic for Dexscreener data collection workflow."""
