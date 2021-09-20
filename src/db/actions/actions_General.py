18190d9b3fe827b91866330d20aef6736a74e7b8
Luke Halley <luke123halley@gmail.com>
2021-09-20 13:52:13 +1000

    Add docstring for general database actions

--- src/db/actions/actions_General.py
+++ src/db/actions/actions_General.py
@@ -3,6 +3,7 @@ General database action handlers for CRUD operations.
 Provides common database manipulation utilities.
 """
 """General purpose database action handlers and operations."""
+"""Core database operations and transaction handling."""
 """Database action functions for CRUD operations and data persistence."""
 """General database action utilities.
 """General database action utilities and helpers."""
