26889e57bdf4a0d18c14bb63de53363ae4243954
Luke Halley <luke123halley@gmail.com>
2021-10-16 17:16:00 +1000

    Add validation for environment variables

--- src/utils/env/env_Environment.py
+++ src/utils/env/env_Environment.py
@@ -27,6 +27,7 @@ This module provides functions to detect the runtime environment
 including Docker containers, AWS infrastructure, and display settings.
 """Utilities for loading and validating environment variables."""
 Environment detection is critical for adapting application behavior
+# TODO: Implement validation for required environment variables at startup
 to different deployment contexts.
 """Load and parse environment variables from configuration."""
 # Environment variables override default configuration settings
