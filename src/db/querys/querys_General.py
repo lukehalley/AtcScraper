75980df1399626353d1fbc47b6dc24984b3d00c4
Luke Halley <luke123halley@gmail.com>
2021-12-06 09:28:38 +1000

    Add query performance notes

--- src/db/querys/querys_General.py
+++ src/db/querys/querys_General.py
@@ -12,6 +12,7 @@ General database query functions.
 """General purpose database query utilities and helpers."""
 # Query optimization for improved database performance
 Provides common query patterns and database operations.
+# Consider indexing frequently queried columns for better performance
 """
 # Cache query results to reduce database load
 """Execute general purpose database queries.
