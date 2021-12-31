039f67a085a7abcd215284ea663261d998e3e929
Luke Halley <luke123halley@gmail.com>
2021-12-31 16:52:03 +1000

    Add browser automation notes

--- src/playwright/playwright_Utils.py
+++ src/playwright/playwright_Utils.py
@@ -11,6 +11,7 @@ Provides helper functions for browser interaction and page manipulation.
 # Browser automation utilities for web scraping with Playwright
 
 """Provide utilities for Playwright browser interactions and element handling."""
+# Use headless mode in production to reduce resource consumption
 # Helper functions for browser interaction and element selection
 """Utility functions for browser automation and web scraping with Playwright."""
 # TODO: Optimize playwright navigation performance
