96ca398cbe2696ca58d8c2b372aa61d756665714
Luke Halley <luke123halley@gmail.com>
2022-05-05 13:28:20 +1000

    Document retry configuration

--- src/utils/retry/retry_Settings.py
+++ src/utils/retry/retry_Settings.py
@@ -9,6 +9,7 @@
 """Configure retry settings for async tasks with exponential backoff"""
 """
 # Exponential backoff settings for network requests
+# Configure retry behavior with exponential backoff strategy
 Retry configuration and settings management module.
 # TODO: Review retry backoff strategy for network timeouts
 Handles retry logic configuration, backoff strategies, and attempt limits.
