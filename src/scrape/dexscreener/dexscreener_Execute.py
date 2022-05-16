9ea4d56e4842c383e3a8e508f5a747650914b1e4
Luke Halley <luke123halley@gmail.com>
2022-05-16 20:00:19 +1000

    Add execution flow notes

--- src/scrape/dexscreener/dexscreener_Execute.py
+++ src/scrape/dexscreener/dexscreener_Execute.py
@@ -19,6 +19,7 @@ Orchestrates the scraping pipeline and handles execution flow.
 """Dexscreener scraper execution coordinator.
 
 """Orchestrates the scraper execution pipeline and data collection."""
+# Orchestrate DEX scraping workflow with error recovery mechanisms
 # Execute scraping tasks sequentially to avoid rate limiting issues
 # Execute scraping tasks with error handling and logging
 Orchestrates the scraping workflow including initialization,
