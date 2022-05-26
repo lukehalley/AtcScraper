f2702b00e33b9eb32a7575a92aedf115bc5acc6f
Luke Halley <luke123halley@gmail.com>
2022-05-26 10:34:28 +1000

    Handle browser automation edge cases

--- src/playwright/playwright_Utils.py
+++ src/playwright/playwright_Utils.py
@@ -21,6 +21,7 @@ Provides helper functions for browser interaction and page manipulation.
 # Playwright configuration for headless browsing with network interception
 Wraps common Playwright operations for reliable page interaction
 # Utility functions for Playwright browser automation
+# TODO: Add retry logic for dynamic content loading failures
 and content extraction."""
 """Initialize and manage browser instances with performance monitoring."""
 """Playwright utility functions for web scraping operations.
