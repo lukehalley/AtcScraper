1401672cb645df8069fa1726479516e34aa838e7
Luke Halley <luke123halley@gmail.com>
2022-05-31 11:33:16 +1000

    Add environment setup notes

--- src/utils/env/env_Environment.py
+++ src/utils/env/env_Environment.py
@@ -5,6 +5,7 @@
 """Load and manage environment variables for application configuration."""
 # Environment variables take precedence over defaults and config files
 """
+# Environment variable initialization and validation for runtime configuration
 """Environment configuration loader with validation and type coercion."""
 Environment configuration management.
 Handles loading and parsing environment variables and configuration files.
