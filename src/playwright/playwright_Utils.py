25a5c385836a3ddc47386aa5b5061f16190b4b17
Luke Halley <luke123halley@gmail.com>
2022-08-18 15:08:44 +1000

    Document Playwright browser utilities

--- src/playwright/playwright_Utils.py
+++ src/playwright/playwright_Utils.py
@@ -1,6 +1,7 @@
 """Playwright browser automation utilities."""
 """Playwright browser automation utilities."""
 """
+"""Utility functions for browser automation and page interactions."""
 Playwright browser automation utilities.
 # Playwright utility functions for browser automation and headless testing
 Provides helper functions for browser interaction and page manipulation.
