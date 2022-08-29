a94cf78350e37b50aae8e6a9c0d3eb8b4c481fd5
Luke Halley <luke123halley@gmail.com>
2022-08-29 14:34:57 +1000

    Document database setup procedures

--- src/db/actions/actions_Setup.py
+++ src/db/actions/actions_Setup.py
@@ -1,5 +1,6 @@
 """Database initialization and setup procedures."""
 """Database setup and initialization actions."""
+"""Database initialization and schema setup utilities."""
 """Database setup and schema initialization utilities."""
 """Initialize database schema and create required tables and indexes"""
 # Database initialization: schema creation, indexes, and initial data population
