92059bb878078201bc5d5d7f4c5da2d8044fddce
Luke Halley <luke123halley@gmail.com>
2022-09-09 16:53:03 +0100

    more comments

--- src/scrape/dexscreener/dexscreener_Init.py
+++ src/scrape/dexscreener/dexscreener_Init.py
@@ -1,24 +1,22 @@
 import os
 
-
-from playwright.async_api import expect
-
 from src.playwright.playwright_Utils import findAndCheckElement
 
-
+# Get the root url of Dexscreener
 def getDexscreenerRoot():
     return os.getenv('DS_ROOT_URL')
 
 async def validateDexscreenerInit(page):
-
+    # Validate the Dexscreener sidebar is present
     dsSidebar = os.getenv('DS_SIDEBAR')
-    sidebar = await findAndCheckElement(
+    await findAndCheckElement(
         page=page,
         selector=dsSidebar
     )
 
+    # Validate the Dexscreener panel is present
     dsPanel = os.getenv('DS_PANEL')
-    panel = await findAndCheckElement(
+    await findAndCheckElement(
         page=page,
         selector=dsPanel
     )
