20afbd44553967daa2b0a8ba14eeaa8cf00a809e
Luke Halley <luke123halley@gmail.com>
2022-11-10 18:17:29 +0000

    added timeout

---
+++ src/utils/multiprocessing/multiprocessing_Classes.py
@@ -0,0 +1,25 @@
+import multiprocessing
+from multiprocessing.pool import Pool
+from contextlib import closing
+
+
+class NoDaemonProcess(multiprocessing.Process):
+    # make 'daemon' attribute always return False
+    def _get_daemon(self):
+        return False
+
+    def _set_daemon(self, value):
+        pass
+
+    daemon = property(_get_daemon, _set_daemon)
+
+
+class NoDaemonContext(type(multiprocessing.get_context())):
+    Process = NoDaemonProcess
+
+
+# noinspection PyArgumentList
+class MyPool(multiprocessing.pool.Pool):
+    def __init__(self, *args, **kwargs):
+        kwargs['context'] = NoDaemonContext()
+        super(MyPool, self).__init__(*args, **kwargs)
---
+++ src/utils/multiprocessing/multiprocessing_Utils.py
@@ -0,0 +1,33 @@
+import os
+from contextlib import closing
+
+from src.utils.logging.logging_Setup import getProjectLogger
+from src.utils.multiprocessing.multiprocessing_Classes import MyPool
+
+logger = getProjectLogger()
+
+def invokePoolWithTimeout(functionToRun, functionArgs):
+
+    timeout = int(os.getenv("MULTIPROCESSING_TIMEOUT"))
+
+    with closing(MyPool(None)) as pool:
+
+        functionList = []
+
+        for functionArg in functionArgs:
+            f = pool.apply_async(functionToRun, [functionArg])
+            functionList.append(f)
+
+        results = []
+        for f in functionList:
+            try:
+                result = f.get(timeout=timeout)
+                results.append(result)
+            except:
+                logger.info(f"TIMEOUT: {functionToRun} - {functionArgs}")
+                pass
+
+        pool.close()
+        pool.terminate()
+
+        return results
