0d613ea6ff6a5774f1eb1184b222e28168f07869
Luke Halley <luke123halley@gmail.com>
2022-11-14 12:34:28 +0000

    added debug

--- src/chain/decode/decode_Tx.py
+++ src/chain/decode/decode_Tx.py
@@ -71,32 +71,32 @@ def uploadTx(transactionDetails):
 
 def decodeTx(transactionDetails):
 
-    #printLog(f"1. DECODING TRANSACTION: {transactionDetails}")
+    printLog(f"1. DECODING TRANSACTION: {transactionDetails}")
     try:
 
         pairDetails = getPairByDbId(
             pairDbId=transactionDetails["pair_id"]
         )
 
-        #printLog(f"2. PAIR DETAILS: {pairDetails}")
+        printLog(f"2. PAIR DETAILS: {pairDetails}")
 
         networkDetails = getNetworkByDbId(
             networkDbId=transactionDetails["network_id"]
         )
 
-        #printLog(f"3. NETWORK DETAILS: {networkDetails}")
+        printLog(f"3. NETWORK DETAILS: {networkDetails}")
 
         dexDetails = getDexByDbId(
             dexDbId=transactionDetails["dex_id"]
         )
 
-        #printLog(f"4. DEX DETAILS: {dexDetails}")
+        printLog(f"4. DEX DETAILS: {dexDetails}")
 
         routerAbi = loadLocalABI(
             path=dexDetails["router_s3_path"]
         )
 
-        #printLog(f"5. GOT ROUTER ABI")
+        printLog(f"5. GOT ROUTER ABI")
 
         ###############################################
         # Pair Details
@@ -105,19 +105,19 @@ def decodeTx(transactionDetails):
         pairDbId = pairDetails["pair_id"]
         pairName = pairDetails["name"]
 
-        #printLog(f"6. PAIR VARS: pairName: {pairDbId} pairName: {pairName}")
+        printLog(f"6. PAIR VARS: pairName: {pairDbId} pairName: {pairName}")
 
         # Token
         tokenInDbId = pairDetails["primary_token_id"]
         tokenOutDbId = pairDetails["secondary_token_id"]
 
-        #printLog(f"7. TOKEN VARS: tokenInDbId: {tokenInDbId} tokenOutDbId: {tokenOutDbId}")
+        printLog(f"7. TOKEN VARS: tokenInDbId: {tokenInDbId} tokenOutDbId: {tokenOutDbId}")
 
         # Network
         networkDbId = networkDetails["network_id"]
         rpcUrl = networkDetails["chain_rpc"]
 
-        #printLog(f"8. NETWORK VARS: networkDbId: {networkDbId} rpcUrl: {rpcUrl}")
+        printLog(f"8. NETWORK VARS: networkDbId: {networkDbId} rpcUrl: {rpcUrl}")
 
         # Dex
         dexDbId = dexDetails["dex_id"]
@@ -125,41 +125,41 @@ def decodeTx(transactionDetails):
         # Router
         routerAddress = dexDetails["router"].replace("\r", "")
 
-        #printLog(f"9. DEX VARS: dexDbId: {dexDbId} routerAddress: {routerAddress}")
+        printLog(f"9. DEX VARS: dexDbId: {dexDbId} routerAddress: {routerAddress}")
 
         ###############################################
         # Tx Details
         ###############################################
         transactionHash = transactionDetails["transaction_hash"]
 
-        #printLog(f"10. transactionHash: {transactionHash}")
+        printLog(f"10. transactionHash: {transactionHash}")
 
         routeId = None
 
-        #printLog(f"11. In Try")
+        printLog(f"11. In Try")
         web3 = Web3(Web3.HTTPProvider(rpcUrl))
-        #printLog(f"12. Got Web 3")
+        printLog(f"12. Got Web 3")
         web3.middleware_onion.inject(geth_poa_middleware, layer=0)
-        #printLog(f"13. Added Middleware")
+        printLog(f"13. Added Middleware")
         transaction = web3.eth.get_transaction(transactionHash)
-        #printLog(f"14. Got Transaction")
+        printLog(f"14. Got Transaction")
         if transaction["to"] == routerAddress:
 
-            #printLog(f"15. Transaction To Matches Router Address")
+            printLog(f"15. Transaction To Matches Router Address")
             inputData = transaction["input"]
-            #printLog(f"16. Got Input Data")
+            printLog(f"16. Got Input Data")
             blockNumber = int(transaction["blockNumber"])
-            #printLog(f"17. Got Block Number")
+            printLog(f"17. Got Block Number")
             (contract, routerAbi) = getContract(routerAddress, json.dumps(routerAbi))
-            #printLog(f"18. Got Router Contract")
+            printLog(f"18. Got Router Contract")
             func_obj, func_params = contract.decode_function_input(inputData)
-            #printLog(f"19. DECODED CONTRACT!")
+            printLog(f"19. DECODED CONTRACT!")
             target_schema = [a['inputs'] for a in routerAbi if 'name' in a and a['name'] == func_obj.fn_name][0]
-            #printLog(f"20. Got Schema")
+            printLog(f"20. Got Schema")
             decoded_func_params = convertToHex(func_params, target_schema)
-            #printLog(f"21. Decoded Params")
+            printLog(f"21. Decoded Params")
             timestamp = web3.eth.getBlock(blockNumber).timestamp
-            #printLog(f"22. Got Timestamp")
+            printLog(f"22. Got Timestamp")
             decodedTransaction = {
                 "name": func_obj.fn_name,
                 "params": decoded_func_params,
@@ -168,35 +168,35 @@ def decodeTx(transactionDetails):
                 "txHash": transaction["hash"],
                 "timestamp": timestamp
             }
-            #printLog(f"23. Created decodedTransaction Object {decodedTransaction}")
+            printLog(f"23. Created decodedTransaction Object {decodedTransaction}")
             routeUsed = decodedTransaction["params"]["path"]
-            #printLog(f"24. Got Route Used")
+            printLog(f"24. Got Route Used")
             tokenInAddress = routeUsed[0]
-            #printLog(f"25. tokenInAddress {tokenInAddress}")
+            printLog(f"25. tokenInAddress {tokenInAddress}")
             tokenOutAddress = routeUsed[-1]
-            #printLog(f"26. tokenOutAddress {tokenOutAddress}")
+            printLog(f"26. tokenOutAddress {tokenOutAddress}")
             isLoopRoute = tokenInAddress == tokenOutAddress
-            #printLog(f"27. isLoopRoute {isLoopRoute}")
+            printLog(f"27. isLoopRoute {isLoopRoute}")
             if not isLoopRoute:
-                #printLog(f"28. Not Loop Route")
+                printLog(f"28. Not Loop Route")
                 routeObject = {
                     "method": decodedTransaction["name"],
                     "route": "-".join(routeUsed),
                     "blockNumber": decodedTransaction["blockNumber"]
                 }
-                #printLog(f"29. routeObject {routeObject}")
+                printLog(f"29. routeObject {routeObject}")
                 if "amountIn" in decodedTransaction["params"]:
                     routeObject["amountIn"] = decodedTransaction["params"]["amountIn"]
                 else:
                     routeObject["amountIn"] = None
-                #printLog(f"30. Added amountIn {routeObject['amountIn']}")
+                printLog(f"30. Added amountIn {routeObject['amountIn']}")
                 if "amountOutMin" in decodedTransaction["params"]:
                     routeObject["amountOutMin"] = decodedTransaction["params"]["amountOutMin"]
                 else:
                     routeObject["amountOutMin"] = None
-                #printLog(f"31. Added amountOutMin {routeObject['amountOutMin']}")
+                printLog(f"31. Added amountOutMin {routeObject['amountOutMin']}")
 
-                #printLog(f"32. Executing addRouteToDB...")
+                printLog(f"32. Executing addRouteToDB...")
                 routeId = addRouteToDB(
                     networkDbId=networkDbId,
                     dexDbId=dexDbId,
@@ -213,22 +213,22 @@ def decodeTx(transactionDetails):
                     amountIn=routeObject["amountIn"],
                     amountOut=routeObject["amountOutMin"]
                 )
-                #printLog(f"33. Executed addRouteToDB!")
+                printLog(f"33. Executed addRouteToDB!")
 
                 if routeId is not None and routeId > 0:
 
-                    #printLog(f"34. Route Added To DB {routeObject} - routeId: {routeId}")
+                    printLog(f"34. Route Added To DB {routeObject} - routeId: {routeId}")
 
                     printLog(
                         msg=f"Added Route For {pairName} ✅"
                     )
 
-                    #printLog(f"35. Updating Pair Analysis...")
+                    printLog(f"35. Updating Pair Analysis...")
                     updatePairAnalysisByDbId(
                         pairDbId=pairDbId,
                         analysisStatus=True
                     )
-                    #printLog(f"36. Updated Pair Analysis!")
+                    printLog(f"36. Updated Pair Analysis!")
 
         return routeId
 
