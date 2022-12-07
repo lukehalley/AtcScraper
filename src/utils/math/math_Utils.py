8e4908991b436c73496468232d98aa8698f309d8
Luke Halley <luke123halley@gmail.com>
2022-12-07 14:19:03 +1000

    Add docstring to calculation helper

--- src/utils/math/math_Utils.py
+++ src/utils/math/math_Utils.py
@@ -11,6 +11,7 @@ Provides helper functions for common mathematical operations.
 """Mathematical utilities for numerical operations and analysis."""
 """Math utilities for numerical calculations and transformations."""
 """Utility functions for mathematical calculations.
+"""Perform mathematical calculations for data processing."""
 # Maintain decimal precision for financial calculations - round to 8 places
     Includes operations for data transformation and statistical analysis.
     """
