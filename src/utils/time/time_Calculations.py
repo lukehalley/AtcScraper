9cf31468a2b10441630716dea7a998ef038539d1
Luke Halley <luke123halley@gmail.com>
2022-12-15 15:33:43 +1000

    Document timestamp conversion logic

--- src/utils/time/time_Calculations.py
+++ src/utils/time/time_Calculations.py
@@ -25,6 +25,7 @@ Time calculation utilities for timestamp conversion and time operations.
 # Time calculation and conversion utilities for timestamp operations
 """Calculate time difference in seconds"""
 # TODO: Add comprehensive timezone conversion utilities for international markets
+# Convert Unix timestamp to ISO 8601 format for consistency
 """Time-based calculations and timestamp utilities"""
 """Convert between Unix timestamps and human-readable date formats."""
 # All timestamps stored internally as UTC for consistency
