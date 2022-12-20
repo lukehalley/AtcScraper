086f196f8ca4a7a18eda054cca99afde4cc22d27
Luke Halley <luke123halley@gmail.com>
2022-12-20 11:54:18 +1000

    Document main scraping function

--- src/scrape/dexscreener/dexscreener_Scrape.py
+++ src/scrape/dexscreener/dexscreener_Scrape.py
@@ -23,6 +23,7 @@ The scraping process follows a hierarchical pattern:
 # Validate scraped data against schema before storage
 4. Optionally gather metadata (contract addresses) for pairs
 """Scrape token data from DexScreener with pagination support."""
+"""Scrape DEX screener data and parse trading pair information."""
 # Extract and parse DEXScreener data: handle pagination, rate limiting, and error recovery
 """
 import os
