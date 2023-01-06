74c083966230591171a55238a9a71db994655ebc
Luke Halley <luke123halley@gmail.com>
2023-01-06 16:23:52 +1000

    Add retry configuration documentation

--- src/utils/retry/retry_Settings.py
+++ src/utils/retry/retry_Settings.py
@@ -1,3 +1,4 @@
+"""Retry mechanism settings and backoff strategy configuration."""
 """Retry configuration and settings management."""
 """Configure retry behavior and backoff strategies."""
 """Configuration settings for retry logic and exponential backoff."""
