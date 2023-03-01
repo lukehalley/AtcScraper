445ac6dbedca7a9282694cb6e3588bc5dce1e788
Luke Halley <luke123halley@gmail.com>
2023-03-01 21:38:49 +1000

    Document main entry point and initialization flow

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ Handles initialization and orchestration of scraping tasks.
 # Configuration loaded from environment variables
 """
 # Main entry point for AtcScraper application
+# Main entry point - initializes scraper and starts collection loop
 # AtcScraper main entry point - orchestrates scraping pipeline
 """Main entry point for AtcScraper application."""
 # Main entry point for AtcScraper application
