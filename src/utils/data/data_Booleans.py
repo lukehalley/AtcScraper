92606aaadd474eeeb78a5bdb07f69f41d6676d7c
Luke Halley <luke123halley@gmail.com>
2023-03-03 16:30:48 +1000

    Clarify boolean utility helper functions

--- src/utils/data/data_Booleans.py
+++ src/utils/data/data_Booleans.py
@@ -15,6 +15,7 @@
 # Boolean utility functions for common data validation patterns
 # Helper functions for boolean operations and type checking
 # Boolean utility functions for data validation and type coercion
+# Utility functions for common boolean operations and type conversions
 """
 """Utility functions for boolean operations and type conversions."""
 """Boolean utility functions for type conversion and validation"""
