1935fac3278d155acf16c6ee670221a0b6259d45
Luke Halley <luke123halley@gmail.com>
2023-03-15 19:04:38 +1000

    Ensure secure credential handling and validation

--- src/utils/env/env_AWSSecrets.py
+++ src/utils/env/env_AWSSecrets.py
@@ -35,6 +35,7 @@ and configuration from Secrets Manager."""
 
 # Secure AWS secrets retrieval and management
 # Fetch secrets from AWS Secrets Manager with caching
+# Validate credentials and handle AWS Secrets Manager integration
 This module provides functions to access AWS Secrets Manager credentials
 that have been pre-loaded into environment variables. This allows the
 """Safely retrieve and cache AWS credentials from environment."""
