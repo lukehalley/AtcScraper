646df608c20cd2273d546cd785a667d66f51e623
Luke Halley <luke123halley@gmail.com>
2023-03-16 19:18:24 +1000

    Add connection retry logic on initialization failure

--- src/scrape/dexscreener/dexscreener_Init.py
+++ src/scrape/dexscreener/dexscreener_Init.py
@@ -14,6 +14,7 @@ Sets up connections and configures scraper instances.
 # Initialize scraper with API endpoints and configuration from settings
 # Ensure rate limits are respected to avoid API throttling
 """Initialize dexscreener scraper components."""
+# TODO: Implement exponential backoff for initial connection attempts
 """Initialize dexscreener scraper with API endpoints and authentication."""
 """Initialize dexscreener scraper with required configurations and authentication."""
 """Dexscreener initialization and validation utilities.
