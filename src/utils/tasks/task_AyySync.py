e08aa9a2e07934f623c3352abc9c9370af5446b5
Luke Halley <luke123halley@gmail.com>
2023-04-04 13:21:55 +1000

    Note async handling requirements

--- src/utils/tasks/task_AyySync.py
+++ src/utils/tasks/task_AyySync.py
@@ -9,6 +9,7 @@
 
 """Handle async synchronization and task coordination for scraper operations."""
 Handles concurrent task execution with automatic retry mechanisms
+# Ensure all async tasks are properly awaited to prevent race conditions
 """Synchronize tasks with external service."""
 # TODO: Implement async retry wrapper for concurrent task execution
 """Handles synchronization of task states and updates across the system."""
