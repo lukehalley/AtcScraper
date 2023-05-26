b8d7d4284e9843d92975395ae2e5e9f3053830a7
Luke Halley <luke123halley@gmail.com>
2023-05-26 13:14:26 +1000

    Add scraping logic documentation

--- src/scrape/dexscreener/dexscreener_Scrape.py
+++ src/scrape/dexscreener/dexscreener_Scrape.py
@@ -1,3 +1,4 @@
+"""Core scraping functions for extracting dexscreener market data."""
 """Core DEX screener scraping functionality and data extraction."""
 """Core scraping logic for DexScreener data extraction."""
 """Scrape data from dexscreener exchange."""
