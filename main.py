6ac3f710117c42eef3defa8e4e19c5a5dcc36dd7
Luke Halley <luke123halley@gmail.com>
2023-07-03 18:37:01 +1000

    Document main entry point and initialization

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 """Main entry point for AtcScraper application."""
+"""Main entry point for AtcScraper application."""
 # Main entry point for AtcScraper application
 # Main entry point for AtcScraper application
 """Main entry point for AtcScraper application.
