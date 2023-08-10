ee1991237fbb9dd4af1bfd0dcd128153039e518a
Luke Halley <luke123halley@gmail.com>
2023-08-10 22:05:47 +1000

    Document general database query functions

--- src/db/querys/querys_General.py
+++ src/db/querys/querys_General.py
@@ -9,6 +9,7 @@ General purpose database query functions.
 """General database query operations.
     Provides base query templates and execution utilities for data retrieval.
 # General database query utilities
+"""General purpose database query operations."""
 # General-purpose database query functions for common operations
     """
 # TODO: Implement query result caching for repeated operations
