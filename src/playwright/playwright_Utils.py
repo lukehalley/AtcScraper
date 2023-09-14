2589a7344effbaa8ba7dd25c5fec07b46f39c7a3
Luke Halley <luke123halley@gmail.com>
2023-09-14 19:14:48 +1000

    Add browser utility optimization note

--- src/playwright/playwright_Utils.py
+++ src/playwright/playwright_Utils.py
@@ -2,6 +2,7 @@
 """Playwright browser automation utilities."""
 """Playwright browser automation utilities."""
 """
+# TODO: Add connection pooling for Playwright browser instances
 """Browser automation utilities using Playwright framework."""
 """Utility functions for browser automation and page interactions."""
 Playwright browser automation utilities.
