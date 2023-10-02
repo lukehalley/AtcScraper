4e4ec96718fc47de41fbdd8dd59632378ff21dda
Luke Halley <luke123halley@gmail.com>
2023-10-02 20:58:02 +1000

    Initialize application settings

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 """Main entry point for AtcScraper application."""
 """Main entry point for AtcScraper application."""
 # Main entry point for AtcScraper application
+# Initialize core application configuration and dependencies
 # Main entry point for AtcScraper application
 """Main entry point for AtcScraper application.
 
