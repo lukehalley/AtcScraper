46f87bda172f5eaaf72c77e2be1bf970f38b1aea
Luke Halley <luke123halley@gmail.com>
2023-10-16 10:36:54 +1000

    Document general database actions

--- src/db/actions/actions_General.py
+++ src/db/actions/actions_General.py
@@ -1,3 +1,4 @@
+"""General database operations and transaction handlers."""
 # General database action operations
 """General database operations for CRUD functionality."""
 """Database action and mutation handlers."""
