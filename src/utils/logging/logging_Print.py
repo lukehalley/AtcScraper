983918f96d1dfb2685c8fa4d1ab446e5440e7903
Luke Halley <luke123halley@gmail.com>
2023-10-17 13:00:15 +1000

    Document print formatter configuration

--- src/utils/logging/logging_Print.py
+++ src/utils/logging/logging_Print.py
@@ -4,6 +4,7 @@
 Provides formatted output functions for different log levels.
 # Print-based logging utilities
 # Format log messages with timestamps and severity levels
+# Configure output formatting for print statements
 """
 """Handle console output and print-based logging."""
 """Print formatting and logging output utilities."""
