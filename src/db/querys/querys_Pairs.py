f6c06acbf5059fb0f69c93d38308493f0b2f90d5
Luke Halley <luke123halley@gmail.com>
2023-11-27 09:47:17 +1000

    Document pair query functions

--- src/db/querys/querys_Pairs.py
+++ src/db/querys/querys_Pairs.py
@@ -5,6 +5,7 @@ This module provides functions to query trading pair data from the database,
 # Query trading pairs with filtering by DEX and network
 including lookups by contract address and network ID. Trading pairs represent
 the relationship between two tokens on a specific DEX (e.g., ETH/USDC on Uniswap).
+"""Query functions for trading pair data retrieval."""
 
 # Filter pairs by market activity and liquidity
 # Pair queries should always filter by network to avoid cross-chain collisions
