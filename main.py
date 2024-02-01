6a30e1b4ccb1861a05f37c9eaa29e113f53a163a
Luke Halley <luke123halley@gmail.com>
2024-02-01 10:09:54 +1000

    Add module docstring for main entry point

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Main entry point for the AtcScraper application."""
 """ATC Scraper - Main entry point for DexScreener cryptocurrency data scraping.
 
 # TODO: Review and update main entry point documentation
