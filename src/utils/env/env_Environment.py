7d8efa22d499e59512f5cbe84c852469ce31064d
Luke Halley <luke123halley@gmail.com>
2025-02-18 10:20:00 +0800

    Add docstrings and extract env variable constants

--- src/utils/env/env_Environment.py
+++ src/utils/env/env_Environment.py
@@ -2,13 +2,45 @@ import os
 
 from src.utils.data.data_Booleans import strToBool
 
-# Check if we are running in a docker container
-def checkIsDocker():
-    return strToBool(os.environ.get("RUNNING_IN_DOCKER"))
+# Environment variable names
+DOCKER_ENV_VAR = "RUNNING_IN_DOCKER"
+AWS_REGION_ENV_VAR = "AWS_DEFAULT_REGION"
+FORCE_HEADLESS_ENV_VAR = "FORCE_HEADLESS"
 
-# Check if we are running on AWS
-def checkIsAWS():
-    return True if os.environ.get("AWS_DEFAULT_REGION") else False
 
-def checkHeadless():
-    return checkIsDocker() or strToBool(os.getenv("FORCE_HEADLESS"))
+def checkIsDocker() -> bool:
+    """
+    Check if the application is running inside a Docker container.
+
+    Returns:
+        bool: True if RUNNING_IN_DOCKER environment variable is set to a truthy value.
+    """
+    return strToBool(os.environ.get(DOCKER_ENV_VAR, False))
+
+
+def checkIsAWS() -> bool:
+    """
+    Check if the application is running on AWS infrastructure.
+
+    Determines AWS environment by checking for the presence of the
+    AWS_DEFAULT_REGION environment variable, which is automatically
+    set in AWS Lambda and other AWS compute services.
+
+    Returns:
+        bool: True if AWS_DEFAULT_REGION is set, False otherwise.
+    """
+    return bool(os.environ.get(AWS_REGION_ENV_VAR))
+
+
+def checkHeadless() -> bool:
+    """
+    Determine if the browser should run in headless mode.
+
+    The browser runs headless when either:
+    - Running inside a Docker container (no display available)
+    - FORCE_HEADLESS environment variable is set to true
+
+    Returns:
+        bool: True if browser should run in headless mode.
+    """
+    return checkIsDocker() or strToBool(os.getenv(FORCE_HEADLESS_ENV_VAR, False))
