5ce220d37332b97255457483d7501c2582d109d3
Luke Halley <luke123halley@gmail.com>
2025-02-18 10:12:33 +0800

    Add type hints and docstring to math_Utils.py

--- src/utils/math/math_Utils.py
+++ src/utils/math/math_Utils.py
@@ -1,4 +1,5 @@
-def replaceTrailingDigitsWithZeros(number):
+def replaceTrailingDigitsWithZeros(number: int) -> int:
+    """Replace all trailing digits with zeros, keeping only the leading digit."""
     leadingNumber = str(number)[0]
     zerosToAdd = len(str(number)) - 1
     return int(f"{leadingNumber}{'0' * zerosToAdd}")
